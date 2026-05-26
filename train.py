76d999ceca4dee3e6d56f5b4e90b7b2a0b297d31
ryan <behruzanorboev1@gmail.com>
2026-05-26 16:24:21 +0900

    feat: completed task M

--- train.py
+++ train.py
@@ -1,23 +1,49 @@
+# TASK M
+
+# Array ichidagi har bir raqam uchun raqamning o'zi va uning kvadratidan tashkil topgan object hosil qilib qaytarsin.
+
+# Masalan: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, ...]
+
+
+# JAVOB:
+
+
+def getSquareNumbers(a):
+    result = []
+
+    for num in a:
+        obj = {
+            "numb": num,
+            "squr": num ** 2
+        }
+        result.append(obj)
+
+    return result
+
+
+print(getSquareNumbers([1, 2, 3]))
+
+
 # TASK L
 
 # So'zlarni ketma-ketligini buzmasdan har bir so'zni alohida teskarisiga o'girib beradigan function tuzing.
 
 # Masalan: reverseSentence("we like coding!") return "ew ekil !gnidoc"
 
-message = "we like coding!"
+# message = "we like coding!"
 
 
-def reverseSentence(a):
-    message = a.split(" ")
-    new_words = []
-    for word in message:
-        new_words.append(word[::-1])
+# def reverseSentence(a):
+#     message = a.split(" ")
+#     new_words = []
+#     for word in message:
+#         new_words.append(word[::-1])
 
-    return " ".join(new_words)
+#     return " ".join(new_words)
 
 
-reverseSentence("we like coding!")
-print(reverseSentence("we like coding!"))
+# reverseSentence("we like coding!")
+# print(reverseSentence("we like coding!"))
 
 #      // JAVOB:
 
