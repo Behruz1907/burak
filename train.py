6a10103a4d65ba9dfac3d6977f6c69715416ef21
ryan <behruzanorboev1@gmail.com>
2026-05-26 11:14:01 +0900

    feat: integration of environmental variables

--- train.py
+++ train.py
@@ -31,3 +31,25 @@ print(reverseSentence("we like coding!"))
 
 # // const yakun = getReverse("assalomu alaykum")
 # // console.log(yakun)
+# // console.log("EXUCUTED!")
+
+# // import moment from 'moment'
+
+# // const currentTime = moment().format('YYYY MM DD')
+# // console.log(currentTime)
+
+# // const person: string = "Ryan"
+# // const count: number = 100
+
+
+# // Architectural pattern: MVC, DI, MVP
+# // bizni backendimiznig suyagi backenddagi malumotlar oqimini tartibga soladigan vositachi
+
+# // MVC == == Module View Controller = > Asosan bu backenda ishlatiladi
+# // Dependency injection Architectural pattern == > Asosan bu modullar ishaltish tizimisda ishlatiladi(NESTJS)
+
+
+# // Design pattern: Middleware, Decorator
+# // backenddi malum bir bolaklarini strukturasini yechishda xizmat qiladiagn pattern
+
+# // Design pattern: 3 qismga yani creational, structural, behaivoral bolimlarga bolidani
