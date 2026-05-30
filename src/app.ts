af4dda5a527568bbc32c483dd833cb644bace1d2
ryan <behruzanorboev1@gmail.com>
2026-05-30 11:41:02 +0900

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import router from './router';
+import routerAdmin from "./routerAdmin"
 
 /** 1-ENTRANCE **/
 const app = express();
@@ -14,6 +15,6 @@ app.use(express.json());
 app.set('views', path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 /** 4-ROUTERS **/     
-app.use('/', router);  // Middleware Design Pattern
-
+app.use('/admin', routerAdmin);  // SPA: React 1- burak bssr di react loyihamizga rest api server sifatida ishlatamiz
+    // SSR: EJS  2- adminka loyihamizdi qurish maqsadida ham ishlatamiz!
 export default app;  // 
