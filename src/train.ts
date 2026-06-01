d164658e52ad345f0b4e305d38ae9b2805f375a7
ryan <behruzanorboev1@gmail.com>
2026-06-01 20:13:15 +0900

    feat: create project standards

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,15 @@
 console.log("Hello World!")
+
+/* Projects Standarts:
+- Logging standarts
+- Naming standarts:
+ function, method, variable =>  CAMEL CASE            goHome
+class => PASCAL CASE                                  MemberService
+ folder => KEBAB CASE
+ css => SNAKE                                         button_style
+
+
+-Error handling
+
+
+*/
