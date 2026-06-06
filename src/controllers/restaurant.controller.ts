29016f9edf6ec6af1110c42eddf8fd7c4ba69642
ryan <behruzanorboev1@gmail.com>
2026-06-06 16:58:58 +0900

    feat: frontend related css, js and image creation

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -7,15 +7,15 @@ import { MemberType } from "../libs/enums/member.enum";
 
    const memberService = new MemberService();
 
-const restaurantController: T = {};
-restaurantController.goHome = (req: Request, res: Response) => {
+   const restaurantController: T = {};
+   restaurantController.goHome = (req: Request, res: Response) => {
   try {
     console.log("goHome");
     // res types => send | json | redirect | end | render
     // Logic
     // Service Model
     // ...
-    res.send("Home Page");
+    res.render("home");
   } catch (err) {
     console.log("Error, goHome:", err);
   }
@@ -24,7 +24,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 restaurantController.getSignup = (req: Request, res: Response) => {
   try {
     console.log("getSignup");
-    res.send("Signup Page");
+    res.render("signup");
   } catch (err) {
     console.log("Error, getSignup:", err);
   }
@@ -34,7 +34,7 @@ restaurantController.getSignup = (req: Request, res: Response) => {
 restaurantController.getLogin = (req: Request, res: Response) => {
   try {
     console.log("getLogin");
-    res.send("Login Page");
+    res.render("login");
   } catch (err) {
     console.log("Error, getLogin:", err);
   }
