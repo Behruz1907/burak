c4b0794b1c370f2aaf154afdf18691f75299ccaa
ryan <behruzanorboev1@gmail.com>
2026-06-12 19:51:38 +0900

    fix: completed mittask

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -4,8 +4,6 @@ import { T } from "../libs/types/common";
 import ProductService from "../models/Product.service";
 
 
-
-
 const productService = new ProductService();
 
 const productController: T = {};
