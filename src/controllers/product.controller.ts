597e840a2d696436d4c01a38916577a85e2b1b38
ryan <behruzanorboev1@gmail.com>
2026-06-14 20:07:30 +0900

    feat: createNewProduct business logic by restaurant member

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,13 +1,18 @@
 import express, { Request, Response } from "express";
-import Errors from "../libs/Error";
+import Errors, { HttpCode, Message } from "../libs/Error";
 import { T } from "../libs/types/common";
 import ProductService from "../models/Product.service";
+import { ProductInput } from "../libs/types/product";
+import { AdminRequest } from "../libs/types/member";
 
 
 const productService = new ProductService();
 
 const productController: T = {};
-
+/*SPA */
+    
+/*SSR */
+    
 productController.getAllProducts = async (req: Request, res: Response) => {
   try {
       console.log("getAllProducts");
@@ -19,15 +24,28 @@ productController.getAllProducts = async (req: Request, res: Response) => {
   }
 };
 
-productController.createNewProduct = async (req: Request, res: Response) => {
+productController.createNewProduct = async (req: AdminRequest, res: Response) => {
   try {
-      console.log("createNewProduct");
-      res.send("DONE!");
+    console.log("createNewProduct");
+    console.log("req.files:", req.files);
 
-   } catch (err) {
-      console.log("Error, createNewProduct:", err);
-      if (err instanceof Errors) res.status(err.code).json(err);
-      else res.status(Errors.standart.code).json(Errors.standart);
+    if (!req.files?.length)
+      throw new Errors(HttpCode.INTERNAL_SEVER_ERROR, Message.CREATED_FAILED); 
+
+    const data: ProductInput = req.body;
+    data.productImages = req.files?.map(ele => {
+      return ele.path.replace(/\\/g, "/");
+    });
+
+    await productService.createNewProduct(data);
+
+    res.send(`<script> alert("Successful creation!"); window.location.replace('admin/product/all) </script>`);
+
+  } catch (err) {
+    console.log("Error, createNewProduct:", err);
+    const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+     res.send(`<script> alert("${message}"); window.location.replace('admin/product/all) </script>`);
+    
   }
 };
 
--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,3 +1,5 @@
+import Errors, { HttpCode, Message } from "../libs/Error";
+import { Product, ProductInput } from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
 
 class ProductService {
@@ -6,6 +8,24 @@ class ProductService {
     constructor() {
         this.productModel = ProductModel;
     }
+
+    /*SPA */
+    
+    /*SSR */
+
+    public async createNewProduct(input: ProductInput): Promise<Product> {
+         try {
+             return await this.productModel.create(input);
+    } 
+         catch (err) {
+             console.log("Error, model:createNewProduct:", err);
+      // Provide error details and a status code to Errors constructor
+      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATED_FAILED);
+    }
+    }
+
 }
+
+  
 
 export default ProductService;
